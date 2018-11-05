904ff805eeb7438ae4ea1681a9eef4139b3b5115
ParmarManpreet <manpreet.parmar29@hotmail.com>
2018-11-05 18:04:59 -0500

    commit server 80% done

--- Server/src/server/MasterServer.java
+++ Server/src/server/MasterServer.java
@@ -7,125 +7,149 @@ import java.awt.Color;
 
 import java.io.*;
 
-public class MasterServer {
+public class MasterServer{
     public  ServerFrame serverFrame;
     public ServerSocket welcomeSocket;
     public Socket connectionSocket;
-    public DataOutputStream outputToServer;
+    public Socket[] clients = new Socket[50];
+    public DataOutputStream outputToClient;
     public BufferedReader inputFromClient;
-    public int clientNumb;
-    
+    public int clientNumb = 0;
+    public Thread thread = null;
+    public String clientNames[] = new String[50];
+
 	public MasterServer() {
 		ServerUI();
-		Server();
-		clientNumb= 0;
+		RunServer();
 	}
 	
 	public void ServerUI() {
+		//make GUI
 		serverFrame = new ServerFrame();
 	}
 	
-	public void Server() {
+	public void RunServer() {
 		try {
-			//port #
 			welcomeSocket = new ServerSocket(2245);
-			while(true) {
-				connectionSocket = welcomeSocket.accept();
-				//reads message sent by client
-				inputFromClient = new BufferedReader
-						(new InputStreamReader(connectionSocket.getInputStream()));
-				String recSentence = inputFromClient.readLine();
-				System.out.println(recSentence);
-				
-				confirmConnect(recSentence);
-				
-//				if(recSentence != null){
-//					
-//					new Thread() {
-//						public void run() {
-//							serverFrame.serverLabel.setText(recSentence);
-//							System.out.println(recSentence);
-//						}	
-//					}.start();
-//				}
-		      	// If connection request, reply to the client 
-		      	// Else clear server frame if there are no clients
-//		      	if(recSentence.startsWith("Disconnection"))
-//		        {
-		        	// pushF1.defaultConnection();
-	 
-//		        }
-//		      	else
-//		        {
-		 
-		//            pushF1.writeinBox(recSentence);
-		//			pushF1.setConnection("Connected");
-		//			System.out.println(recSentence);
-				
-		            //send message to the client
-		           // DataOutputStream outputToClient = new 
-		                   // DataOutputStream(connectionSocket.getOutputStream());
-		
-		            //use \n bc we use readline before and it looks for new line character
-		         //   String sentence= "From Server: Connected..." +'\n';
-		
-		          // outputToClient.writeBytes(sentence);
-		            
-		        }
-				
-			//}
-		//we can't close loop because we have an infinite loop to keep socket open
-		//welcomeSocket.close();
-//		
 	    }catch(Exception ex) {}
+		
+		try {
+			while(true) {
+				//accept client trying to connect
+				connectionSocket = welcomeSocket.accept();
+				clientThread(connectionSocket);			    
+		    }
+		}catch(Exception ex){}
 	}
 	
-	private void confirmConnect(String connect) {
-		if(connect.equals("Connected")) {
-			try {
-				updateServerUI(connect);
-	            //send message to the client
-	            DataOutputStream outputToClient;
-				outputToClient = new 
-				        DataOutputStream(connectionSocket.getOutputStream());
-	            //use \n bc we use readline before and it looks for new line character
-	            String sentence= "You are Connected" +'\n';
-	            outputToClient.writeBytes(sentence);
-			} catch (IOException e) {}
-		}else {
-			updateServerUI(connect);	
+	public void notifyConnection(Socket connectionSocket, String message) {
+		try {
+			DataOutputStream outputToClient = new DataOutputStream(connectionSocket.getOutputStream());
+	        outputToClient.writeBytes(message);
+		}catch(Exception ex) {}
+	}
+	
+	public void clientThread(Socket connectionSocket){
+		try {
+			inputFromClient = new BufferedReader
+					(new InputStreamReader(connectionSocket.getInputStream()));
+			String input = inputFromClient.readLine();
+			createThread(connectionSocket, input);
+			notifyConnection(connectionSocket, "You are Connected" + "\n");
+			accounceConnection(input + " is connected"+ "\n", connectionSocket);
+			
+			new Thread() {
+				public void run() {
+					while(true) {
+						String message;
+						try {
+							message = inputFromClient.readLine();
+							chatWithClients(message + "\n", connectionSocket);
+							if(message.equals("Disconnected")) {
+								removeClient(connectionSocket);
+								break;
+							}
+						} catch (IOException e) {
+						}
+					}
+				}
+			}.start();
+		}catch(Exception ex) {}
+	}	
+	
+	public void removeClient(Socket connectionSocket){
+		clientNumb--;
+		updateServerUI();
+		for(int i=0;i<clientNumb+1; i++){
+			if(clients[i]==connectionSocket) {
+				clients[i]=null;
+				clientNames[i]=null;
+			}
+		}
+	}
+
+	public void accounceConnection(String message, Socket connectionSocket2) {		
+		for(int i=0;i<clientNumb+1; i++) {
+			if(clients[i] != null && clients[i] != connectionSocket) {
+				Socket socket = clients[i];
+			
+				try {
+					DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+			        outputToClient.writeBytes(message);
+				} catch (Exception ex) {}
+			}
+		}
+	}
+
+	public void createThread(Socket connectionSocket, String name){
+		System.out.println("inside createThread clientNumb " + clientNumb);
+		clients[clientNumb]= connectionSocket;
+		clientNames[clientNumb++] = name;
+		updateServerUI();
+	}
+
+	public void chatWithClients(String message, Socket connectionSocket) {
+		System.out.println(clients[clientNumb-1]+ " ");	
+		String name = null;
+		for(int i=0; i<clientNumb+1; i++) {
+			if(clients[i]==connectionSocket) {
+				name = clientNames[i];
+				break;
+			}	
+		}
+		
+		for(int i=0;i<clientNumb+1; i++) {
+			if(clients[i] != connectionSocket) {
+				System.out.println("value of i " + i);
+				Socket socket = clients[i];
+				System.out.println("socket value being sent " + socket);
+			
+				try {
+					DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+			        outputToClient.writeBytes(name + ": " + message);
+				} catch (Exception ex) {}
+			}
 		}
 	}
 
 	//updates server UI by adding or removing number of clients connected
-	public void updateServerUI(String connect){
-		if(connect.equals("Connected")) {
-			clientNumb++;
-			if(clientNumb == 1) {
-				serverFrame.serverLabel.setText(clientNumb + " Client Connected" );
-				serverFrame.serverLabel.setForeground(Color.BLUE);
-			}else {
-			serverFrame.serverLabel.setText(clientNumb + "Clients Connected" );
+	public void updateServerUI(){
+		if(clientNumb == 1) {
+			serverFrame.serverLabel.setText(clientNumb + " Client Connected" );
 			serverFrame.serverLabel.setForeground(Color.BLUE);
-			}
-			
-		}else if (connect.equals("Disconnected")) {
-			clientNumb--;
-			if(clientNumb == 1) {
-				serverFrame.serverLabel.setText(clientNumb + " Client Connected" );
-				serverFrame.serverLabel.setForeground(Color.BLUE);
-			}else if(clientNumb==0){
-				serverFrame.serverLabel.setText("No Clients Connected" );
-				serverFrame.serverLabel.setForeground(Color.RED);
-			}else {
-			serverFrame.serverLabel.setText(clientNumb + "Clients Connected" );
-			serverFrame.serverLabel.setForeground(Color.BLUE);
-			}
+		}else if (clientNumb == 0) {
+			serverFrame.serverLabel.setText("No Clients Connected");
+			serverFrame.serverLabel.setForeground(Color.RED);	
+		}else {
+		serverFrame.serverLabel.setText(clientNumb + "Clients Connected" );
+		serverFrame.serverLabel.setForeground(Color.BLUE);
 		}
 	}
 	public static void main(String[] args) {
 		MasterServer runServer= new MasterServer();
 
 }
+
+
 
 }
