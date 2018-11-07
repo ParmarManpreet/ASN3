b24a412c2d8b04401d853393275ca1d746b804c0
ParmarManpreet <manpreet.parmar29@hotmail.com>
2018-11-07 16:27:03 -0500

    oldserver

--- Server/src/server/MasterServer.java
+++ Server/src/server/MasterServer.java
@@ -51,24 +51,30 @@ public class MasterServer{
 	
 	public void clientThread(Socket connectionSocket){
 		try {
+			System.out.println("Socket number: "+ connectionSocket);
 			inputFromClient = new BufferedReader
 					(new InputStreamReader(connectionSocket.getInputStream()));
 			String input = inputFromClient.readLine();
-			createThread(connectionSocket, input);
+			System.out.println("input: "+input);
+			createArray(connectionSocket, input);
+			//notifies the client connecting that he/she is connected
 			notifyConnection(connectionSocket, "You are Connected" + "\n");
+			//announce all the other clients that a new client has connected
 			accounceConnection(input + " is connected"+ "\n", connectionSocket);
 			
+			//this thread is constantly listening to client messages
 			new Thread() {
 				public void run() {
 					while(true) {
 						String message;
 						try {
 							message = inputFromClient.readLine();
-							chatWithClients(message + "\n", connectionSocket);
 							if(message.equals("Disconnected")) {
+								System.out.println("socket we want to delete " + connectionSocket);
 								removeClient(connectionSocket);
 								break;
 							}
+							chatWithClients(message, connectionSocket);
 						} catch (IOException e) {
 						}
 					}
@@ -101,36 +107,69 @@ public class MasterServer{
 		}
 	}
 
-	public void createThread(Socket connectionSocket, String name){
-		System.out.println("inside createThread clientNumb " + clientNumb);
+	public void createArray(Socket connectionSocket, String name){
+		System.out.println("name into array: " + clientNumb);
 		clients[clientNumb]= connectionSocket;
+		System.out.println("confirm name name into array: " + name);
 		clientNames[clientNumb++] = name;
 		updateServerUI();
 	}
-
+	
+	//send message to all clients
 	public void chatWithClients(String message, Socket connectionSocket) {
-		System.out.println(clients[clientNumb-1]+ " ");	
 		String name = null;
-		for(int i=0; i<clientNumb+1; i++) {
-			if(clients[i]==connectionSocket) {
-				name = clientNames[i];
-				break;
-			}	
-		}
-		
-		for(int i=0;i<clientNumb+1; i++) {
-			if(clients[i] != connectionSocket) {
-				System.out.println("value of i " + i);
-				Socket socket = clients[i];
-				System.out.println("socket value being sent " + socket);
+		Socket socket = clients[0];
+		if(message.startsWith("YES")) {
+				String[] data = message.split(",",3);
+				name = data[1];
+				System.out.println("sending DM to: " + name);
+				message = data[2];
+				System.out.println("message of DM: " + message);
+				for(int i = 0; i<clientNumb; i++) {
+					System.out.println("value of i in loop: " + i);
+					System.out.println("is it the right receiver of the DM: " + clientNames[i]+ "");
+					if(clientNames[i].equals(name)) {
+						System.out.println("if statement == name of receiver is: " + clientNames[i]);
+						socket = clients[i];
+						System.out.println("socket number of the receiver " + clients[i]);
+						break;
+					}
+				}
+				for(int i = 0; i<clientNumb; i++) {
+					if(clients[i] == connectionSocket) {
+						name = clientNames[i];
+						System.out.println("sent by: " + name);
+						break;
+					 }
+				}
+			try {
+				System.out.println(socket);
+				String newMessage = name + ": " + message + "\n";
+				DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+		        outputToClient.writeBytes(newMessage);
+			} catch (Exception ex) {}
+		}else {
+			for(int i=0; i<clientNumb+1; i++) {
+				//System.out.println("Value of i "+ i);
+				//System.out.println("Number of clients "+ clientNumb);
+				if(clients[i]==connectionSocket) {
+					name = clientNames[i];
+					break;
+				}	
+			}
 			
-				try {
-					DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-			        outputToClient.writeBytes(name + ": " + message);
-				} catch (Exception ex) {}
+			for(int i=0;i<clientNumb+1; i++) {
+				if(clients[i] != connectionSocket) {
+					Socket socketcon = clients[i];
+					try {
+						String eMessage = name + ": " + message + "\n"; 
+						DataOutputStream outputToClient = new DataOutputStream(socketcon.getOutputStream());
+				        outputToClient.writeBytes(eMessage);
+					} catch (Exception ex) {}
+				}
 			}
 		}
-	}
+	}	
 
 	//updates server UI by adding or removing number of clients connected
 	public void updateServerUI(){
